d54e3bdd03f98c79e2ed4ca0c95123d938af36e5
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-26 13:39:35 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { createContext, useContext } from "react";
+export const BoardCtx = createContext(null);
+export const useBoard = () => {
+  const ctx = useContext(BoardCtx);
+  if (!ctx) throw new Error("useBoard must be within <BoardProvider>");
+  return ctx;
+};
