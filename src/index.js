8fa0897b567213f1ec0d8fc83ad4b008dfb4a277
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-29 13:33:55 +0200

    Updated barrel file.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-import { createContext, useContext } from "react";
-export const BoardCtx = createContext(null);
-export const useBoard = () => {
-  const ctx = useContext(BoardCtx);
-  if (!ctx) throw new Error("useBoard must be within <BoardProvider>");
-  return ctx;
-};
+export * from "./useBoard.jsx";
